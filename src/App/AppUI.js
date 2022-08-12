7f17e29dfbf055b1ad0e9bfdb1c00dfd86ad2754
JhonDev365 <jhonarcila365@gmail.com>
2022-08-12 16:29:38 -0500

    react context

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TodoContex } from "../TodoContext";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
@@ -6,49 +7,38 @@ import { CreateTodoButton} from '../CreateTodoButton'
 import { TodoItem } from "../TodoItem";
 import { ProgressBar } from "../ProgressBar";
 
-function AppUI({
-    error,
-    loading,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-}) {
+function AppUI() {
     return (
     //react solo acepta un componente, por lo cual
     //renderizamos una etiqueta invisible --> React.fragment
     <React.Fragment>
-        <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-        />
-        <ProgressBar
-        total={totalTodos}
-        completed={completedTodos}
-        />
-        <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        />
-        <TodoList>
-        {error && <p>Desespérate, hubo un Error.</p>}
-        {loading && <p>Estamos cargando, NO desespéres ...</p>}
-        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer ToDo!</p>}
+        <TodoCounter/>
+        <ProgressBar/>
+        <TodoSearch/>
 
-        {searchedTodos.map(todo => (
-            <TodoItem
-            //al renderizar elementos de una lista,a rray, tenemso que usar
-            // la propiedad key a nuestros componentes para que react los pueda identificar
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}/>
-        ))}
-        </TodoList>
+        <TodoContex.Consumer>
+            {({error, loading, searchedTodos, completeTodo, deleteTodo})  => 
+                (
+                    <TodoList>
+                        {error && <p>Desespérate, hubo un Error.</p>}
+                        {loading && <p>Estamos cargando, NO desespéres ...</p>}
+                        {(!loading && !searchedTodos.length) && <p>¡Crea tu primer ToDo!</p>}
+
+                        {searchedTodos.map(todo => (
+                            <TodoItem
+                            //al renderizar elementos de una lista,a rray, tenemso que usar
+                            // la propiedad key a nuestros componentes para que react los pueda identificar
+                            key={todo.text}
+                            text={todo.text}
+                            completed={todo.completed}
+                            onComplete={() => completeTodo(todo.text)}
+                            onDelete={() => deleteTodo(todo.text)}/>
+                        ))}
+                    </TodoList>
+                )
+            }
+        </TodoContex.Consumer>
+        
         <CreateTodoButton/>
     </React.Fragment>
     );
